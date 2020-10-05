76a189d24391b2611b0a0d3bf505c864c9564303
Brian Ray Xu <33497224+brianrayxu@users.noreply.github.com>
2020-10-05 17:56:25 -0400

    Update tweetAPIexample.py

--- tweetAPIexample.py
+++ tweetAPIexample.py
@@ -19,18 +19,17 @@ def Write_tweets_to_File(Input_list,target_filename):
     filename = "%s.json" % target_filename
     Tweets_text = open(filename, 'w') 
     for status in Input_list:
-        # json.dump(status._json,Tweets_text,indent = 4)
         data.append(status._json)
     json.dump(data,Tweets_text)
     Tweets_text.close
 
 #Search tweets based on Hashtag and time.
-def GET_Hashtag_Search_Tweets(Local_API,Hashtag,Count_Number,Time_before):
-    Hashtag_Tweets = tweepy.Cursor(Local_API.search,q=Hashtag,count=Count_Number,since=Time_before)
+def QueryTweets(API,Hashtag,Count_Number,since_id):
+    Hashtag_Tweets = tweepy.Cursor(API.search,q=Hashtag,count=Count_Number,since=since_id)
     Write_tweets_to_File(Hashtag_Tweets.items(),'Hashtag_Tweets')
 
 if __name__ == "__main__":
     API = Authorization_Setup()
     API.wait_on_rate_limit=True
     # the hashtag has been switched to '#BU' in order to avoid twitter API error 429 
-    GET_Hashtag_Search_Tweets(API,"#Boston University",5,"2020-9-28")
+    QueryTweets(API,"#Boston University",5,"2020-9-28")
