b46192a4e17ef0a951c452599a2a14e844ce0eb6
Brian Ray Xu <33497224+brianrayxu@users.noreply.github.com>
2020-11-05 00:23:23 -0500

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,20 @@
+import pytest
+import tweetAPIexample as API
+import sentiment_analysis as NLP
+import os
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
+
+api = API.Authorization_Setup()
+with open('Hashtag_Tweets.json') as f:
+  testdata = json.load(f)
+
+#standard unit test of search function of API using return value for successful query
+def standard_test_API():
+    assert API.GET_Hashtag_Search_Tweets(api,"#BU","recent",5,"2020-11-1") == testdata,"Error")
+
+#Checking if there is information present to analyze
+def sentiment_test_empty():
+    assert os.path.getsize("./Hashtag_tweets.json") != 0, "File w/ Tweets is empty"
