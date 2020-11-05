e08c0987dc9f4debc53432cdeaf702ad734d4479
Brian Ray Xu <33497224+brianrayxu@users.noreply.github.com>
2020-11-05 00:21:56 -0500

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -4,26 +4,27 @@ from google.cloud.language import enums
 from google.cloud.language import types
 import json
 
-# Instantiates a client
-client = language.LanguageServiceClient()
+def calc_sentiment():
+    # Instantiates a client
+    client = language.LanguageServiceClient()
 
-#Import Tweets result file.
-with open("Hashtag_Tweets.json", "r+", encoding='utf-8') as f:
-    data = json.loads(f.read()) 
-Sentiment_output = open("tweets_sentiment.txt","w", encoding='utf-8')
-#Analyze each tweet text
-generalScore = 0.0
-for tweet in data:
-    text = tweet['text']
-    document = types.Document(
-        content=text,
-        type=enums.Document.Type.PLAIN_TEXT)
-    #Call API to analyze text.
-    sentiment = client.analyze_sentiment(document=document).document_sentiment
-    #Write result to file
-    generalScore += sentiment.score
-    Sentiment_output.write("Text:" + text +"\n")
-    Sentiment_output.write("Sentiment score (score, magnitude): {}, {}".format(sentiment.score, sentiment.magnitude)+"\n\n")
+    #Import Tweets result file.
+    with open("Hashtag_Tweets.json", "r+", encoding='utf-8') as f:
+        data = json.loads(f.read()) 
+    Sentiment_output = open("tweets_sentiment.txt","w", encoding='utf-8')
+    #Analyze each tweet text
+    generalScore = 0.0
+    for tweet in data:
+        text = tweet['text']
+        document = types.Document(
+            content=text,
+            type=enums.Document.Type.PLAIN_TEXT)
+        #Call API to analyze text.
+        sentiment = client.analyze_sentiment(document=document).document_sentiment
+        #Write result to file
+        generalScore += sentiment.score
+        Sentiment_output.write("Text:" + text +"\n")
+        Sentiment_output.write("Sentiment: {}, {}".format(sentiment.score, sentiment.magnitude)+"\n\n")
 
-print("The general sentiment score of Trump over last 20 tweets is: " + str(generalScore))
-
+    print("The general sentiment score of Trump over last 20 tweets is: " + str(generalScore))
+    return generalScore
